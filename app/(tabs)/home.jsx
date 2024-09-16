88f583d24fd3164dc2d16fe30b2739be014626e0
Aprendiz <Aprendiz@BOGDFPCGMF4145.FormacionSena.RED>
2024-09-16 14:23:43 -0500

    Se realizo el trabajo completo con funcionalidad añadida nueva

--- app/(tabs)/home.jsx
+++ app/(tabs)/home.jsx
@@ -1,10 +1,26 @@
 import React, { useContext, useEffect, useState } from 'react';
 import { GlobalContext } from '../../context/GlobalProvider'; // Asumiendo que manejas autenticación global
-import { View, Text } from 'react-native';
+import { SafeAreaView } from 'react-native-safe-area-context';
+import { FlatList, Image, RefreshControl, Text, View } from 'react-native';
+
+import { images } from '../../constants';
+import useAppwrite from '../../lib/useAppwrite';
+import { getAllPosts, getLatestPosts } from '../../lib/appwrite';
+import { EmptyState, SearchInput, Trending, VideoCard } from '../../components';
 
 const Home = () => {
   const { user } = useContext(GlobalContext); // Suponiendo que `user` es parte del contexto global
+  const { data: posts, refetch } = useAppwrite(getAllPosts);
+  const { data: latestPosts } = useAppwrite(getLatestPosts);
+
+  const [refreshing, setRefreshing] = useState(false);
   const [dateTime, setDateTime] = useState(new Date());
+
+  const onRefresh = async () => {
+    setRefreshing(true);
+    await refetch();
+    setRefreshing(false);
+  };
 
   // Función para actualizar la fecha y hora
   useEffect(() => {
@@ -15,15 +31,68 @@ const Home = () => {
   }, []);
 
   return (
-    <View style={{ padding: 20 }}>
-      <Text style={{ fontSize: 24, fontWeight: 'bold' }}>
-        {user ? `¡Bienvenido, ${user.name}!` : 'Bienvenido a la plataforma'}
-      </Text>
-      <Text style={{ marginTop: 10, fontSize: 18 }}>
-        {`Fecha y hora actual: ${dateTime.toLocaleDateString()} ${dateTime.toLocaleTimeString()}`}
-      </Text>
-      {/* Aquí va el resto del contenido de la página */}
-    </View>
+    <SafeAreaView className="bg-primary">
+      <FlatList
+        data={posts}
+        keyExtractor={(item) => item.$id}
+        renderItem={({ item }) => (
+          <VideoCard
+            title={item.title}
+            thumbnail={item.thumbnail}
+            video={item.video}
+            creator={item.creator.username}
+            avatar={item.creator.avatar}
+          />
+        )}
+        ListHeaderComponent={() => (
+          <View className="flex my-6 px-4 space-y-6">
+            <View className="flex justify-between items-start flex-row mb-6">
+              <View>
+                <Text className="font-pmedium text-sm text-gray-100">
+                  Welcome Back
+                </Text>
+                <Text className="text-2xl font-psemibold text-white">
+                  {user ? `¡Bienvenido, ${user.name}!` : 'JSMastery'}
+                </Text>
+              </View>
+
+              <View className="mt-1.5">
+                <Image
+                  source={images.logoSmall}
+                  className="w-9 h-10"
+                  resizeMode="contain"
+                />
+              </View>
+            </View>
+
+            <SearchInput />
+
+            <View className="w-full flex-1 pt-5 pb-8">
+              <Text className="text-lg font-pregular text-gray-100 mb-3">
+                Latest Videos
+              </Text>
+
+              <Trending posts={latestPosts ?? []} />
+            </View>
+
+            <View className="mt-4">
+              <Text className="text-lg font-pregular text-gray-100">
+                Fecha y hora actual: {dateTime.toLocaleDateString()} {dateTime.toLocaleTimeString()}
+              </Text>
+            </View>
+          </View>
+        )}
+        ListEmptyComponent={() => (
+          <EmptyState
+            title="No Videos Found"
+            subtitle="No videos created yet"
+          />
+        )}
+        refreshControl={
+          <RefreshControl refreshing={refreshing} onRefresh={onRefresh} />
+        }
+      />
+    </SafeAreaView>
   );
 };
 
